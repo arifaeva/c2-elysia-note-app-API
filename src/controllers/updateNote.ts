9099b1854d30c1e82f71eefd62530cda7ba47151
arifaeva <arifa.eva.ixa@gmail.com>
2025-10-21 21:51:23 +0700

    feat: create updateNote controller

---
+++ src/controllers/updateNote.ts
@@ -0,0 +1,26 @@
+import { Context, t } from "elysia";
+import { prisma } from "../utils/prisma";
+
+export const updateNoteSchema = {
+  body: t.Object({
+    content: t.String(),
+    isDone: t.Boolean(),
+  }),
+};
+
+export const updateNote = async ({ params, body }: Context) => {
+  const { id } = params;
+  const { content, isDone } = body as { content: string; isDone: boolean };
+
+  const updatedNote = await prisma.note.update({
+    where: {
+      id,
+    },
+    data: {
+      content,
+      isDone,
+    },
+  });
+
+  return updatedNote;
+};
